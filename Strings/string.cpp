d3336afccaaed0eb8a88676e124765260dbd691f
Sagar Patil <sagar.22111052@viit.ac.in>
2023-02-15 19:55:04 +0530

    Delete Strings directory

--- Strings/string.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    char name[10]={'S','A','G','A','R','\0'};
-    char name1[]={'S','A','G','A','R','\0'};
-    char name2[]="SAGAR";
-    cout<<name<<" "<<name1<<" "<<name2;
-}
